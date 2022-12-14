9ab1bfdf62b74c8e2fcbdfb336cb0c772a2f0f38
Khalilova <sharova-ira90@yandex.ru>
2022-12-14 14:20:17 +0300

    Reformate code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public float calculate (float weight, float height) {
+    public float calculate(float weight, float height) {
         float index = weight / (height * height);
         return index;
     }
